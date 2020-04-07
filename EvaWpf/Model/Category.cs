f2fdd63a919a08b56c9ed2a6d189192ae2a057a8
adelvig-an <63202963+adelvig-an@users.noreply.github.com>
2020-04-07 21:16:41 +0700

    Добавьте файлы проекта.

---
+++ EvaWpf/Model/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EvaWpf.Model
+{
+    class Category
+    {
+        public string Name { get; set; }
+        public List<BuildingBase> Items { get; set; }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
